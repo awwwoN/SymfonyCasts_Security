0ff8a5466b73915fcd6df37e5e9d6d19cfe4e255
Noa Druesne <noa.druesne7@gmail.com>
2022-11-24 09:07:03 +0100

    Updating login function in SecurityController

--- start/src/Controller/SecurityController.php
+++ start/src/Controller/SecurityController.php
@@ -5,16 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastName = $authenticationUtils->getLastUsername();
+
         return $this->render('security/login.html.twig', [
-            'controller_name' => 'SecurityController',
+            'last_username' => $lastName,
+            'error' => $error,
         ]);
     }
 }
