5e506bcbb6da6898f5c02dcc61e1b4f6bda15985
Noa Druesne <noa.druesne7@gmail.com>
2022-11-24 11:56:38 +0100

    Updating account pages + adding a BaseController

---
+++ start/src/Controller/AccountController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @IsGranted("ROLE_USER")
+ */
+class AccountController extends BaseController
+{
+    /**
+     * @Route("/account", name="app_account")
+     */
+    public function index(LoggerInterface $logger): Response
+    {
+        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
+
+        return $this->render('account/index.html.twig', [
+
+        ]);
+    }
+}
