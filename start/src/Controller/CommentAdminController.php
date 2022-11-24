3513a83e94fff81cfbfdda7691efbe8c0440fd43
Noa Druesne <noa.druesne7@gmail.com>
2022-11-24 11:20:01 +0100

    Security again

--- start/src/Controller/CommentAdminController.php
+++ start/src/Controller/CommentAdminController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class CommentAdminController extends Controller
 {
     /**
