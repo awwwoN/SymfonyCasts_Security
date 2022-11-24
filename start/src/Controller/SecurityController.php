6215b308adb3b4966f4f1ed5fe5e82c89280a5c2
Noa Druesne <noa.druesne7@gmail.com>
2022-11-24 10:21:07 +0100

    Adding csrf token

--- start/src/Controller/SecurityController.php
+++ start/src/Controller/SecurityController.php
@@ -23,4 +23,12 @@ class SecurityController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('will be intercepted before getting here');
+    }
 }
